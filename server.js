e33d430de1a48e624d7d1646c4945868395dfa00
ivanmanan <ivanmanan@gmail.com>
2017-11-18 20:17:45 -0800

    Benchmark

--- server.js
+++ server.js
@@ -47,70 +47,9 @@ var connection = mysql.createConnection({
   database : config.database
 });
 //Connect to Database only if Config.js parameter is set.
-if(config.use_database==='true')
-{
+if(config.use_database==='true') {
   connection.connect();
 }
-
-////////////////////////////////////////////////////////////////////////
-// Passport
-
-
-// Passport session setup.
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-// Use the FacebookStrategy within Passport.
-passport.use(new FacebookStrategy({
-  clientID: config.facebook_api_key,
-  clientSecret:config.facebook_api_secret ,
-      callbackURL: config.callback_url
-    },
-    function(accessToken, refreshToken, profile, done) {
-      process.nextTick(function () {
-        //Check whether the User exists or not using profile.id
-        //Further DB code.
-        return done(null, profile);
-      });
-    }
-  ));
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(cookieParser());
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(session({ secret: 'keyboard cat',  resave: true, saveUninitialized: true, key: 'sid'}));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(express.static(__dirname + '/public'));
-  //Router code
-  app.get('/', function(req, res){
-    res.render('index', { user: req.user });
-  });
-  app.get('/account', ensureAuthenticated, function(req, res){
-    res.render('account', { user: req.user });
-  });
-  //Passport Router
-  app.get('/auth/facebook', passport.authenticate('facebook'));
-  app.get('/auth/facebook/callback',
-    passport.authenticate('facebook', {
-         successRedirect : '/',
-         failureRedirect: '/login'
-    }),
-    function(req, res) {
-      res.redirect('/');
-    });
-  app.get('/logout', function(req, res){
-    req.logout();
-    res.redirect('/');
-  });
-  function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) { return next(); }
-    res.redirect('/login')
-  }
-  //app.listen(3000);
 
 
 
@@ -126,7 +65,7 @@ passport.use(new FacebookStrategy({
 ////////////////////////////////////////////////////////////////////////
 // Post Requests
 
-
+/*
 app.get('/backend', function(req, res, next) {
   res.json([
     {
@@ -146,7 +85,7 @@ app.get('/backend', function(req, res, next) {
     }
   ]);
 });
-
+*/
 
 
 console.log("===================================================");
@@ -155,7 +94,7 @@ console.log("GPS Testing");
 app.post('/coordinates', function(req,res) {
 
   console.log("Running query...");
-
+/*
   var longitude = req.longitude;
   var latitude = req.latitude;
 
@@ -163,7 +102,7 @@ app.post('/coordinates', function(req,res) {
                    + 1 + '", "' + longitude + '", "' + latitude + '");');
 
   res.end("Success!");
-
+*/
 });
 
 
