6ab4cccfb28ff1a9f470d6c8f5f568cbf92cd172
Yong Bum Yoo <yongy0221@gmail.com>
2017-11-18 20:11:24 -0800

    adding

--- skeleton.js
+++ skeleton.js
@@ -2,6 +2,9 @@
 
 module.exports = {
   database:{
+    facebook_api_key      :     "374669916277319",
+    facebook_api_secret   :     "1981e5b93a24a47af9980c8bddc40572",
+    callback_url          :     "http://localhost:3000/auth/facebook/callback",
     host: "127.0.0.1",
     user: "root",
     password: "YourPassword",
